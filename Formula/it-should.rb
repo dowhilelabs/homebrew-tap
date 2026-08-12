e46a0493e2cbdc9ef7644865262ead47b1da1625
Joeseph Rodrigues <dowhilegeek@gmail.com>
2026-08-11 21:06:38 -0700

    it-should: fix release URL to v0.2.0

--- Formula/it-should.rb
+++ Formula/it-should.rb
@@ -2,8 +2,8 @@ class ItShould < Formula
   desc "Turn a plain-English request into one approved shell command"
   homepage "https://github.com/dowhilelabs/it-should"
   version "0.2.0"
-  url "https://github.com/dowhilelabs/it-should/releases/download/v0.1.0/it-aarch64-apple-darwin.tar.gz"
-  # SHA-256 of it-aarch64-apple-darwin.tar.gz (v0.1.0 release).
+  url "https://github.com/dowhilelabs/it-should/releases/download/v0.2.0/it-aarch64-apple-darwin.tar.gz"
+  # SHA-256 of it-aarch64-apple-darwin.tar.gz (v0.2.0 release).
   sha256 "82e9ce80dbfc878990a8ce8b15d373c2c95b5b8833cb494d938c9e6c672547a4"
 
   depends_on :macos
